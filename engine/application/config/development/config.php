1e05b7362c8f462ef8877c7b8aeb64cfdc717ca0
marwansaleh <marwansaleh@192.168.1.46>
2015-04-29 17:09:53 +0700

    Change domain for cookie in config file

--- engine/application/config/development/config.php
+++ engine/application/config/development/config.php
@@ -266,7 +266,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "sism_";
-$config['cookie_domain']	= ".nasabah.co";
+$config['cookie_domain']	= "localhost";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
