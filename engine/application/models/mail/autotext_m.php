530b41c39565da2eb420600823f9cfce739c72db
marwansaleh <marwansaleh@10.25.121.112>
2015-05-01 16:24:30 +0700

    New model for autotexts

---
+++ engine/application/models/mail/autotext_m.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of Autotext_m
+ *
+ * @author Marwan Saleh <marwan.saleh@ymail.com>
+ */
+class Autotext_m extends MY_Model {
+    protected $_table_name = 'autotexts';
+    protected $_primary_key = 'id';
+    protected $_primary_filter = 'intval';
+    protected $_order_by = 'name asc';
+}
+
+/*
+ * file location: engine/application/models/mail/template_m.php
+ */
