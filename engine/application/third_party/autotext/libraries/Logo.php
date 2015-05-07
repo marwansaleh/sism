06b08d7e20d3c03a998d9ba83133d5ad787204ee
marwansaleh <marwansaleh@192.168.88.49>
2015-05-07 18:12:53 +0700

    New image logo lubuklinggau

--- engine/application/third_party/autotext/libraries/Logo.php
+++ engine/application/third_party/autotext/libraries/Logo.php
@@ -7,6 +7,9 @@
 class Logo {
     //must at least has one public method getValue
     function getValue(&$autoText){
+        if (ini_get('allow_url_fopen')!='1'){
+            return;
+        }
         $ci =& get_instance();
         $img_src = site_url('assets/img/lubuklinggau.png');
         $autoText->content = $img_src ;//$ci->load->view('logo', array('logo_data'=>$img_src),TRUE);
