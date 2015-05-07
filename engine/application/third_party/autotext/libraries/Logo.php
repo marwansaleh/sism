3d7c39cb2f9ea15882f00fc2a51fd27207099d01
marwansaleh <marwansaleh@192.168.88.49>
2015-05-07 17:42:36 +0700

    New image logo lubuklinggau

--- engine/application/third_party/autotext/libraries/Logo.php
+++ engine/application/third_party/autotext/libraries/Logo.php
@@ -8,7 +8,7 @@ class Logo {
     //must at least has one public method getValue
     function getValue(&$autoText){
         $ci =& get_instance();
-        $img_src = site_url('assets/img/logo-surat.png');
+        $img_src = site_url('assets/img/lubuklinggau.png');
         $autoText->content = $img_src ;//$ci->load->view('logo', array('logo_data'=>$img_src),TRUE);
     }
 }
