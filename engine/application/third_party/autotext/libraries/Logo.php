5c87707829930a40160612a2337c82f070383164
marwansaleh <marwansaleh@192.168.88.49>
2015-05-07 17:39:37 +0700

    New image logo lubuklinggau

--- engine/application/third_party/autotext/libraries/Logo.php
+++ engine/application/third_party/autotext/libraries/Logo.php
@@ -10,7 +10,7 @@ class Logo {
         $ci =& get_instance();
         //$ci->load->helper('url');
         //$img_src = 'http://google.co.id/logos/doodles/2015/ki-hajar-dewantaras-126th-birthday-4695498065707008.3-res.png';
-        $img_src = site_url('assets/template/img/logo-surat.png');
+        $img_src = site_url('assets/img/logo-surat.png');
         $autoText->content = $img_src ;//$ci->load->view('logo', array('logo_data'=>$img_src),TRUE);
     }
 }
