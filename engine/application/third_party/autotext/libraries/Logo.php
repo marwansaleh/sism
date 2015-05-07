9bb68f578748330887b0e2234bacbf03c656823e
marwansaleh <marwansaleh@192.168.88.49>
2015-05-07 16:53:12 +0700

    New thirdparty for logo surat

---
+++ engine/application/third_party/autotext/libraries/Logo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * AutoText logo
+ *
+ * @author marwansaleh
+ */
+class Logo {
+    //must at least has one public method getValue
+    function getValue(&$autoText){
+        $ci =& get_instance();
+        //$ci->load->helper('url');
+        //$img_src = 'http://google.co.id/logos/doodles/2015/ki-hajar-dewantaras-126th-birthday-4695498065707008.3-res.png';
+        $img_src = site_url('assets/template/img/logo-surat.png');
+        $autoText->content = $img_src ;//$ci->load->view('logo', array('logo_data'=>$img_src),TRUE);
+    }
+}
