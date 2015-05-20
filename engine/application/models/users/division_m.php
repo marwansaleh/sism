63444e87c94a4eac613fa4c9dd79825d8f8c2ec8
marwansaleh <marwansaleh@localhost>
2015-05-20 12:19:44 +0700

    New data model for user division

---
+++ engine/application/models/users/division_m.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of Division_m
+ *
+ * @author Marwan Saleh <marwan.saleh@ymail.com>
+ */
+class Division_m extends MY_Model {
+    protected $_table_name = 'division';
+    protected $_primary_key = 'id';
+    protected $_primary_filter = 'intval';
+    protected $_order_by = 'division';
+    
+}
+
+/*
+ * file location: engine/application/models/users/division_m.php
+ */
