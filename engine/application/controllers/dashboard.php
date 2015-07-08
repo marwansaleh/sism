21c2c727d19e61a7755be37e99e5429a80eac98c
marwansaleh <marwansaleh@localhost>
2015-07-08 20:50:30 +0700

    Udate dashboard

--- engine/application/controllers/dashboard.php
+++ engine/application/controllers/dashboard.php
@@ -28,6 +28,50 @@ class Dashboard extends MY_AdminController {
         $this->data['disposition_send_count'] = $this->disposition_m->get_count(array('sender'=>$me->id));
         $this->data['disposition_receive_count'] = $this->disposition_m->get_count(array('receiver'=>$me->id));
         
+        $latest_num_records = 10;
+        $last_incomings = $this->incoming_m->get_offset('*', $this->users->has_access('INCOMING_VIEW_ALL') ? NULL : array('receiver'=>$me->id),0,$latest_num_records);
+        $this->data['last_incomings'] = array();
+        foreach ($last_incomings as $incoming){
+            $incoming->receiver_name = $incoming->receiver==$me->id ? 'You' : $this->user_m->get_value('full_name', array('id'=>$incoming->receiver));
+            $incoming->status_name = mail_status($incoming->status, MAIL_TYPE_INCOMING, SIDE_RECEIVER);
+            
+            $this->data['last_incomings'] [] = $incoming;
+        }
+        
+        $this->db->where('receiver', $me->id)->or_where('sender', $me->id);
+        $last_dispositions = $this->disposition_m->get_offset('*', NULL,0,$latest_num_records);
+        $this->data['last_dispositions'] = array();
+        foreach ($last_dispositions as $disposition){
+            $disposition->status_name = mail_status($disposition->status, $disposition->mail_type, $disposition->sender==$this->users->get_userid()?SIDE_SENDER:SIDE_RECEIVER);
+            if ($disposition->mail_type==MAIL_TYPE_INCOMING){
+                $disposition->subject = $this->incoming_m->get_value('subject', array('id' => $disposition->mail_id));
+            } else {
+                $disposition->subject = $this->outgoing_m->get_value('subject', array('id' => $disposition->mail_id));
+            }
+            if ($disposition->sender == 0) {
+                if ($disposition->mail_type == MAIL_TYPE_INCOMING) {
+                    $disposition->sender_name = $this->incoming_m->get_value('sender_name', array('id' => $disposition->mail_id));
+                } else {
+                    $disposition->sender_name = '';
+                }
+            } else {
+                $disposition->sender_name = $disposition->sender==$me->id ? 'You' : $this->user_m->get_value('full_name', array('id' => $disposition->sender));
+            }
+            $disposition->receiver_name = $disposition->receiver == $me->id ? 'You' : $this->user_m->get_value('full_name', array('id'=>$disposition->receiver));
+            
+            $this->data['last_dispositions'] [] = $disposition;
+        }
+        
+        $last_outgoings = $this->outgoing_m->get_offset('*', $this->users->has_access('OUTGOING_VIEW_ALL') ? NULL : array('sender'=>$me->id),0,$latest_num_records);
+        $this->data['last_outgoings'] = array();
+        foreach ($last_outgoings as $outgoing){
+            $outgoing->sender_name = $outgoing->sender == $me->id ? 'You' : $this->user_m->get_value('full_name', array('id'=>$outgoing->sender));
+            $outgoing->receiver_name = $outgoing->receiver>0 ? $this->user_m->get_value('full_name', array('id'=>$outgoing->receiver)):$outgoing->literally_receiver;
+            $outgoing->status_name = mail_status($outgoing->status, MAIL_TYPE_OUTGOING, SIDE_SENDER);
+            
+            $this->data['last_outgoings'] [] = $outgoing;
+        }
+        
         //get visitors
         $this->data['user_count'] = $this->user_m->get_count();
         
