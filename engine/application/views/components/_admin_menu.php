dc0067e74b042bd8e42fd50ac0635ca6079b0911
marwansaleh <marwansaleh@localhost>
2015-07-08 23:38:57 +0700

    Update mainmenu based on privileges

--- engine/application/views/components/_admin_menu.php
+++ engine/application/views/components/_admin_menu.php
@@ -6,15 +6,18 @@
     <li <?php echo $active_menu=='disposition'?'class="active"':''; ?>><a href="<?php echo site_url('disposition'); ?>"><i class="fa fa-mail-forward"></i> <span>Surat Disposisi</span></a></li>
     <li <?php echo $active_menu=='outgoing'?'class="active"':''; ?>><a href="<?php echo site_url('outgoing'); ?>"><i class="fa fa-mail-reply"></i> <span>Surat Keluar</span></a></li>
     <li <?php echo $active_menu=='report'?'class="active"':''; ?>><a href="<?php echo site_url('report'); ?>"><i class="fa fa-calendar-o"></i> <span>Laporan</span></a></li>
-    <li <?php echo $active_menu=='template'?'class="active"':''; ?>><a href="<?php echo site_url('template'); ?>"><i class="fa  fa-paragraph"></i> <span>Template</span></a></li>
     
-    <li class="treeview">
+    <?php if ($privileges["TEMPLATE_MANAGEMENT"]): ?>
+    <li <?php echo $active_menu=='template'?'class="active"':''; ?>><a href="<?php echo site_url('template'); ?>"><i class="fa  fa-paragraph"></i> <span>Template</span></a></li>
+    <?php endif; ?>
+<!--    <li class="treeview">
         <a href="#"><i class="fa fa-envelope"></i> <span>Log Email</span>  <i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
             <li><a href="#">Email list</a></li>
             <li><a href="#">Email configuration</a></li>
         </ul>
-    </li>
+    </li>-->
+    <?php if ($privileges["USER_MANAGEMENT"]):?>
     <li class="treeview">
         <a href="#"><i class="fa fa-users"></i> <span>User Managements</span> <i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
@@ -24,12 +27,17 @@
             <li><a href="<?php echo site_url('userroles'); ?>">Access roles</a></li>
         </ul>
     </li>
+    <?php endif;?>
     <li class="treeview">
         <a href="#"><i class="fa fa-cogs"></i> <span>Configuration</span> <i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
+            <?php if ($privileges["SYS_PARAMETERS"]): ?>
             <li><a href="<?php echo site_url('sysconf'); ?>">System configuration</a></li>
+            <?php endif; ?>
             <li><a href="#">System log</a></li>
+            <?php if ($privileges["DATABASE_BACKUP_CREATE"]): ?>
             <li><a href="<?php echo site_url('database'); ?>">Database Backup</a></li>
+            <?php endif; ?>
         </ul>
     </li>
     
